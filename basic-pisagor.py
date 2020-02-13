1913a7732f036a43efdf0b7d3b90dfdf4c16bd17
berkayozlu <52002090+berkayozlu@users.noreply.github.com>
2020-02-13 21:30:05 +0300

    Add files via upload

---
+++ basic-pisagor.py
@@ -0,0 +1,8 @@
+while True:
+	print ("---------The Great Pisagor---------")
+	a = int(input("a degerini giriniz:")) 
+	b = int(input ("b degerini giriniz:"))
+	c = (a**2 + b**2)**0.5
+	alan = a*b/2 
+	print ("c kenari: {} Alani ise {}".format(c,alan))
+
